2d4286ad6b0f837de40f9c58bb4964f5f33d4b55
Conan Tzou <conantzou@gmail.com>
2015-05-19 22:44:12 -0700

    finish phase 5

--- skeleton/lib/phase5/params.rb
+++ skeleton/lib/phase5/params.rb
@@ -10,9 +10,12 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
+      @params = parse_www_encoded_form(req).merge(route_params)
     end
 
     def [](key)
+      value = @params[key.to_sym]
+      value ||= @params[key.to_s]
     end
 
     def to_s
@@ -23,16 +26,51 @@ module Phase5
 
     private
     # this should return deeply nested hash
-    # argument format
+    # argument formate
     # user[address][street]=main&user[address][zip]=89436
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
+      query_string = www_encoded_form.query_string
+      body = www_encoded_form.body
+      return {} if query_string.blank? && body.blank?
+
+      all_params = {}
+
+      [query_string, body].each do |param|
+        next if param.blank?
+
+        URI::decode_www_form(param).each do |(key, val)|
+          # all_params[key] = val
+          parsed_keys = parse_key(key)
+          pkeys_length = parsed_keys.length
+
+          builds = []
+          (pkeys_length - 1).downto(0) do |i|
+            temp_hash = {}
+            current_key = parsed_keys[i]
+            last_level = builds.pop
+
+            if i == pkeys_length - 1
+              temp_hash = { current_key => val }
+            else
+              temp_hash = { current_key => last_level }
+            end
+
+            builds << temp_hash
+          end
+
+          all_params.merge!(builds.last)
+        end
+      end
+      p all_params
+      all_params
     end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
+      keys = key.split(/\]\[|\[|\]/)
     end
   end
 end
